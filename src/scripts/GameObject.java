452ca30158d99f491021b6ddbc018a4ca381e385
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-09-20 22:43:09 -0300

    v-00 : Created GameObject Class
    GameObject function was created, this class intends to control others objects positions, and sizes, and is responsable by checking if an object is intersecting in another one.

---
+++ src/scripts/GameObject.java
@@ -0,0 +1,25 @@
+package scripts;
+
+public class GameObject {
+    private int x; // X coordinate
+    private int y; // Y coordinate
+    private int width; // Width of the object
+    private int height; // Height of the object
+
+    // Constructor
+    public GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    // Method to check if this object is touching another object
+    public boolean isTouching(GameObject otherObject) {
+        // Check if this object's bounding box intersects with the other object's bounding box
+        return this.x < otherObject.x + otherObject.width &&
+               this.x + this.width > otherObject.x &&
+               this.y < otherObject.y + otherObject.height &&
+               this.y + this.height > otherObject.y;
+    }
+}
