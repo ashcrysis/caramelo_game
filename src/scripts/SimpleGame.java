52d0b52c417ae5e3d6f5bd1e73a6db18b882ed99
Adrianogdn <adriano.gomesn@gmail.com>
2023-09-21 13:33:17 -0300

    Update SimpleGame.java
    Acrescentado a troca de forma de do Sprite de acordo com o botão pressionado pelo jogador na forma através de código if/else e switch/case.

--- src/scripts/SimpleGame.java
+++ src/scripts/SimpleGame.java
@@ -7,11 +7,11 @@ import java.awt.event.ActionListener;
 
 //game logic testing on java - Asher Gabriela
 
-
 public class SimpleGame extends JPanel implements ActionListener {
     private int spriteX = 100; // X pos initial position
     private int spriteY = 100;   // Y pos initial position
     private int targetY = 100;   // Y destine position
+    private int animalForm = 0; // (0,1,2) = (dog,bird,fish)  
     private Timer timer;
 
     public SimpleGame() {
@@ -20,7 +20,7 @@ public class SimpleGame extends JPanel implements ActionListener {
 
         // Set up the window size
         setPreferredSize(new Dimension(400, 400));
-
+                      
         // Set the possible positions on the Y Axis
         int[] yPositions = {100, 200, 300};
 
@@ -42,14 +42,46 @@ public class SimpleGame extends JPanel implements ActionListener {
         setFocusable(true);
     }
 
+@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        // Draw the sprite according to the current shape (dog, fish or bird)
+        if (animalForm == 0) {
+            g.setColor(Color.RED);
+            g.fillRect(spriteX, spriteY, 50, 50); // DOG
+        } else if (animalForm == 1) {
+            g.setColor(Color.GREEN);
+            g.fillOval(spriteX, spriteY, 50, 50); // BIRD
+        } else if (animalForm == 2) {
+            g.setColor(Color.BLUE);
+            g.fillOval(spriteX, spriteY, 50, 50); // FISH
+        }
+    }
+/* FORMA UTILIZANDO SWITCH CASE
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Draws the sprite
-        g.setColor(Color.RED);
-        g.fillRect(spriteX, spriteY, 50, 50);
-    }
 
+        switch (animalForm) {
+            case 0: // DOG
+                g.setColor(Color.RED);
+                g.fillRect(spriteX, spriteY, 50, 50);
+                break;
+            case 1: // BIRD
+                g.setColor(Color.GREEN);
+                g.fillOval(spriteX, spriteY, 50, 50);
+                break;
+            case 2: // FISH
+                g.setColor(Color.BLUE);
+                g.fillOval(spriteX, spriteY, 50, 50);
+                break;
+            default:
+                break;
+        }
+    }
+*/
     @Override
     public void actionPerformed(ActionEvent e) {
         // Moves the Sprite to the desired position
@@ -58,7 +90,14 @@ public class SimpleGame extends JPanel implements ActionListener {
         } else if (spriteY > targetY) {
             spriteY = spriteY - 5;
         }
-
+        // Checks the sprite's current position and sets the animal's shape
+        if (spriteY == 100) {
+            animalForm = 1; // BIRD
+        } else if (spriteY == 200) {
+            animalForm = 0; // DOG
+        } else if (spriteY == 300) {
+            animalForm = 2; // FISH
+        }
         repaint();
     }
 
@@ -73,3 +112,5 @@ public class SimpleGame extends JPanel implements ActionListener {
         });
     }
 }
+
+
