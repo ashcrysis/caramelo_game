169c72ad506752eefe0ae298e0c57df2b6da42a1
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2023-11-15 03:38:53 -0300

    v-034: removing unused imports

--- src/scripts/main/SimpleGame.java
+++ src/scripts/main/SimpleGame.java
@@ -5,11 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.util.Random;
-import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
 
 public class SimpleGame extends JPanel implements ActionListener {
     // Player related variables
